f9131e1caf340780462d2acb51fee0eda81d65c8
mauriziopalmisano <maurizio.palmisano1@gmail.com>
2026-05-06 17:49:44 +0200

    aggiunto un campo input per cercare per titolo

--- src/App.jsx
+++ src/App.jsx
@@ -6,31 +6,42 @@ function App() {
   //const emptyfilmOBJ = { title: '', genre: '' };
   //const [filmOBJ, setFilmOBJ] = useState(emptyfilmOBJ);
   const [filmsList, setFilmslist] = useState(filmsArray);
-  const [filteredFilmsList, setFilteredFilmsList] = useState(filmsArray);
+  const [filteredFilmsList, setFilteredFilmsList] = useState(filmsList);
   const [selectedGenre, setSelectedGenre] = useState('');
+  const [searchedTitle, setSearchedTitle] = useState('');
 
 
   const genreList = filmsList
-  .filter((film, index, selfArray) => index === selfArray
-  .findIndex(element => element.genre === film.genre));
+    .filter((film, index, selfArray) => index === selfArray
+      .findIndex(element => element.genre === film.genre));
 
   const changeHandler = (event) => {
     const target = event.target;
     const targetValue = target.value;
-    setSelectedGenre(targetValue);
+    const targetName = target.name;
+    if (targetName === 'genre') {
+      setSelectedGenre(targetValue);
+    } else {
+      setSearchedTitle(targetValue);
+    }
   }
 
 
   useEffect(() => {
-    if(selectedGenre === ''){
-      setFilteredFilmsList(filmsList);
-    } else {
-      const newFilteredFilmsList = filmsList.filter(film => {
+    let newFilteredFilmsList = filmsList;
+    if (selectedGenre !== '') {
+      newFilteredFilmsList = filmsList.filter(film => {
         return film.genre === selectedGenre
       });
-      setFilteredFilmsList(newFilteredFilmsList);
     }
-  },[selectedGenre, filmsList]);
+    if (searchedTitle !== '') {
+      newFilteredFilmsList = newFilteredFilmsList.filter(film => {
+        return film.title.toLowerCase().includes(searchedTitle.toLowerCase())
+      });
+    }
+    
+    setFilteredFilmsList(newFilteredFilmsList);
+  }, [selectedGenre, searchedTitle, filmsList]);
 
 
   return (
@@ -45,21 +56,27 @@ function App() {
       <main className="container-fluid">
         <div className="row justify-content-center">
           <div className='col-6'>
-            <select class="form-select" value={selectedGenre} onChange={changeHandler} >
+            <select class="form-select" value={selectedGenre} onChange={changeHandler} name='genre' >
               <option selected disabled value=''>Scegli un Genere</option>
               <option value=''>Tutti i generi</option>
               {genreList.map(film => {
-                const { id, genre} = film;
+                const { id, genre } = film;
                 return (
-                    <option key={id} value={genre}>{genre}</option>
+                  <option key={id} value={genre}>{genre}</option>
                 )
               })}
             </select>
           </div>
+          <div className='col-6'>
+            <div>
+              <label htmlFor="title-search">Cerca per titolo</label>
+              <input type="text" id='title-search' value={searchedTitle} onChange={changeHandler} name='title' />
+            </div>
+          </div>
         </div>
         <div className="row justify-content-center">
           {filteredFilmsList.map(film => {
-            const { id,title, genre } = film;
+            const { id, title, genre } = film;
             return (
               <div className='col-3 m-2' key={id}>
                 <div class="card">
